ba578e8160bd5b8872b1eeda4fbe7ca00ef4745c
Vishakha Bansal <vishakhabansal16@gmail.com>
2023-02-22 11:05:22 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-ethers");
+//const {ethers} = require("hardhat");
+async function main(){
+  const ContractInstance = await ethers.getContractFactory("ERC20");
+  const contractInstance = await ContractInstance.deploy("ERC20Token", "ERC20T", 18, 1000);
+  await contractInstance.deployed();
+  console.log(contractInstance.address);
+
+}
+main()
+.then(() =>{
+    process.exit(0);
+} )
+.catch((err)=> {
+    console.log(err);
+    process.exit(1);
+} )
+
+
